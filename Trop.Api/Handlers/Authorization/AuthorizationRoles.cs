fd1c520ed77c4ee24cb2a61125fcbe631e14f9bd
CoollbreackerzSdo <ralberinfante4@gmail.com>
2024-10-17 07:15:03 -0400

    create authorization basic polices

---
+++ Trop.Api/Handlers/Authorization/AuthorizationRoles.cs
@@ -0,0 +1,7 @@
+namespace Trop.Api.Handlers.Authorization;
+
+public static class AuthorizationRoles
+{
+  public const string Normal = nameof(Normal);
+  public const string Admin = nameof(Admin);
+}
