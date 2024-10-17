7e9c5b0cfaccc850bafb4f095d56495f45e1b8b1
CoollbreackerzSdo <ralberinfante4@gmail.com>
2024-10-17 07:15:46 -0400

    register validation services

--- Trop.Application/ServiceDiscovery.cs
+++ Trop.Application/ServiceDiscovery.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using FluentValidation;
+
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 
 using Trop.Application.Handlers;
 using Trop.Application.Handlers.Create;
+using Trop.Application.Services.Validators;
 using Trop.Domain.Models.User;
 
 namespace Trop.Application;
@@ -17,6 +20,11 @@ public static partial class ServiceDiscovery
     public static IServiceCollection AddHashers(this IServiceCollection services)
     {
         services.AddTransient<IPasswordHasher<UserEntity>, PasswordHasher<UserEntity>>();
+        return services;
+    }
+    public static IServiceCollection AddValidators(this IServiceCollection services)
+    {
+        services.AddTransient<IValidator<CreateUserCommandHandler>,UserValidator>();
         return services;
     }
 }
