80e56e42250139c8f8927b68ba175c54d2faf3fc
CoollbreackerzSdo <ralberinfante4@gmail.com>
2024-10-18 18:53:16 -0400

    create validation tests

--- Trop.Test/Unit/Validators/CreateUserValidatorTest.cs
+++ Trop.Test/Unit/Validators/CreateUserValidatorTest.cs
@@ -4,31 +4,30 @@ using Trop.Application.Handlers.Create;
 using Trop.Application.Services.Validators;
 
 namespace Trop.Test.Unit.Validators;
-
-public class UserValidatorTest
+public class CreateUserValidatorTest
 {
     [Fact]
     public void CreateValidaUser_ReturnTrue()
     {
         var validator = new CreateUserValidator();
-        var @new = new CreateUserCommandHandler("Marcos Pelado", "cocos@gmail.com", "MINIMO930232kiloc");
+        var @new = new CreateUserCommandHandler("Marcos", "cocos@gmail.com", "MINIMO930232kiloc");
         validator.Validate(@new).IsValid.Should().BeTrue();
     }
     [Fact]
     public void CreateInValidaNameUser_ReturnFalse()
     {
         var validator = new CreateUserValidator();
-        var @new = new CreateUserCommandHandler("Marcos", "cocos@gmail.com", "MINIMO930232kiloc");
+        var @new = new CreateUserCommandHandler("Mas", "cocos@gmail.com", "MINIMO930232kiloc");
         validator.Validate(@new).IsValid.Should().BeFalse();
     }
-        [Fact]
+    [Fact]
     public void CreateInValidaEmailUser_ReturnFalse()
     {
         var validator = new CreateUserValidator();
-        var @new = new CreateUserCommandHandler("Marcos Pelado", "cocogmail.com", "MINIMO930232kiloc");
+        var @new = new CreateUserCommandHandler("Marcos", "cocogmail.com", "MINIMO930232kiloc");
         validator.Validate(@new).IsValid.Should().BeFalse();
     }
-            [Fact]
+    [Fact]
     public void CreateInValidaPasswordUser_ReturnFalse()
     {
         var validator = new CreateUserValidator();
---
+++ Trop.Test/Unit/Validators/ValidateUserValidationTest.cs
@@ -0,0 +1,31 @@
+using FluentAssertions;
+
+using Trop.Application.Handlers.Read;
+using Trop.Application.Services.Validators;
+
+namespace Trop.Test.Unit.Validators;
+
+public class ValidateUserValidationTest
+{
+    [Fact]
+    public void InsertEmptyUserName_ReturnFalse()
+    {
+        var validator = new ValidateUserValidator();
+        var userDate = new ValidateUserCommandHandler("","cocacolase32902SDSA");
+        validator.Validate(userDate).IsValid.Should().BeFalse();
+    }
+    [Fact]
+    public void InsertValidUserDate_ReturnTrue()
+    {
+        var validator = new ValidateUserValidator();
+        var userDate = new ValidateUserCommandHandler("Carcos","cocacolase32902SDSA");
+        validator.Validate(userDate).IsValid.Should().BeTrue();
+    }
+    [Fact]
+    public void InsertEmptyPassword_ReturnFalse()
+    {
+        var validator = new ValidateUserValidator();
+        var userDate = new ValidateUserCommandHandler("Carcos","cocacolase32902SDSA");
+        validator.Validate(userDate).IsValid.Should().BeFalse();
+    }
+}
